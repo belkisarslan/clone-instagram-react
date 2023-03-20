609c81e4b6ea7e80f2ed99d78ec596c86046e1da
belkisarslan <belkisarslan33@gmail.com>
2023-03-20 19:44:37 +0300

    reply bar

--- src/pages/inbox/chat/index.js
+++ src/pages/inbox/chat/index.js
@@ -1,8 +1,7 @@
-
 import Header from './components/Header'
-import React from 'react'
+import Reply from './components/Reply'
 
-const Chat = () => {
+export default function Chat(){
   const user = {
     name: "Cansuu",
     avatar: "https://img-s1.onedio.com/id-5cf9be310a9a1a007bef5970/rev-0/w-600/h-509/f-jpg/s-b251005796b9525ce631c88dd39bf56b035cc448.jpg"
@@ -10,8 +9,10 @@ const Chat = () => {
   return (
     <div className='flex-1'>
       <Header user={user}/>
+      <main className='h-[calc(100%-144px)]'>
+
+      </main>
+      <Reply/>
     </div>
   )
 }
-
-export default Chat
